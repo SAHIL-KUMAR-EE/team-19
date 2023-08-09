af62fdad331312627b3996b5dea780b52b30d31c
SAHIL-KUMAR-EE <121251129+SAHIL-KUMAR-EE@users.noreply.github.com>
2023-08-09 09:35:48 +0530

    Update App.js
    Added Dark Mode toggle button

--- chat/clientfrontend/src/App.js
+++ chat/clientfrontend/src/App.js
@@ -2,6 +2,7 @@ import "./App.css";
 import io from "socket.io-client";
 import { useState } from "react";
 import Chat from "./Chat";
+import white_bg from "./back_w.jpg";
 
 const socket = io.connect("http://192.168.1.46:3001");
 
@@ -17,11 +18,28 @@ function App() {
     }
   };
 
+  const [isClicked, setIsClicked] = useState(false);
+
+  const handleClick = () => {
+    setIsClicked(!isClicked);
+  };
+
   return (
-    <div className="App">
+    <div className="App" style={{backgroundImage: isClicked ? `url(${white_bg})` : ''}}>
       {!showChat ? (
         <div className="joinChatContainer">
-          <h3>GhostTalk</h3>
+          <div id="toggle" onClick={handleClick}
+      style={{
+        backgroundImage: isClicked ? `url(${white_bg})` : '',
+        boxShadow: isClicked ? 'inset 0 2px 60px rgba(0,0,0,0.1),inset 0 2px 8px rgba(0,0,0,0.1),inset 0 -4px 4px rgba(0,0,0,0.05)' : '',
+      }}><i class="indicator" onClick={handleClick}
+      style={{
+        left: isClicked ? '40px' : '',
+        background: isClicked ? 'linear-gradient(to bottom, #eaeaea, #f9f9f9)' : '',
+        boxShadow: isClicked ? 'inset 0 8px 20px rgba(0,0,0,0.1),inset 0 4px 4px rgba(255,255,255,1),inset 0 -4px 4px rgba(255,255,255,1)' : '',
+      }}></i></div>
+          <h3 style={{color: isClicked ? '#2b2b2b':''}}>
+          <img id="logo" src={'ghost_logo.svg'} alt='Logo' ></img>Ghost Talk</h3>
           <input
             type="text"
             placeholder="Username"
@@ -29,6 +47,7 @@ function App() {
               setUsername(event.target.value);
             }}
           />
+          <div id="login_frontend">
           <input
             type="text"
             placeholder="Room ID"
@@ -37,6 +56,7 @@ function App() {
             }}
           />
           <button onClick={joinRoom}>Join Room</button>
+          </div>
         </div>
       ) : (
         <Chat socket={socket} username={username} room={room} />
